5e61e11cfc98ad5b3eaac22bcfaa95e75c9e69ce
Ryan Campione <ryan.campione@gmail.com>
2018-07-20 20:07:00 +0000

    added edit user account page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
     
-    # new user
+    # new user template
     def new
         @user = User.new
     end
     
+    # create a new user in the User table
     def create
         @user = User.new(user_params)
         if @user.save
@@ -15,6 +16,22 @@ class UsersController < ApplicationController
         end
     end
     
+    # edit user template
+    def edit
+        @user = User.find(params[:id])
+    end
+   
+    # update user record in the User table
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          flash[:success] = "Your account was updated successfully"
+          redirect_to articles_path
+          
+        else
+            render 'edit' 
+        end
+    end
     
     private
     def user_params
