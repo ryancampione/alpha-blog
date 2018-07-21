0e80dff3fa12f6dbf5b0369d4c70fa405e01d9ed
Ryan Campione <ryan.campione@gmail.com>
2018-07-21 16:50:33 +0000

    added size option to gravatar_for helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
 
-    def gravatar_for(user)
+    def gravatar_for(user, options = {size: 120})
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+        size = options[:size]
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
         image_tag(gravatar_url, alt: user.username, class: "rounded-circle")
     end
 
