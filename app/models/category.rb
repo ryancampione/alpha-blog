040dfba0dd217e762b38004ef2efbae900a70852
Ryan Campione <ryan.campione@gmail.com>
2018-07-24 13:49:22 +0000

    added article_categories many-to-many relationship

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
+    has_many :article_categories
+    has_many :articles, through: :article_categories
     
     before_save {
         self.name = name.titleize
